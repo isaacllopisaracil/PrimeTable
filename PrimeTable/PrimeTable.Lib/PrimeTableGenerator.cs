e16e6aa3fe93dba8c09789698e4077ca9cddf033
Isaac Llopis <isaac.llopis.aracil@gmail.com>
2015-11-04 22:31:52 +0000

    Implemented PrimeTableGenerator.Generate.

--- PrimeTable/PrimeTable.Lib/PrimeTableGenerator.cs
+++ PrimeTable/PrimeTable.Lib/PrimeTableGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using PrimeTable.Lib.Contracts;
 using System;
+using System.Linq;
 
 namespace PrimeTable.Lib
 {
@@ -19,7 +20,17 @@ namespace PrimeTable.Lib
             if (length <= 0)
                 throw new ArgumentOutOfRangeException($"Method {nameof(Generate)} only accepts {nameof(length)} values greater than 0.");
 
-            throw new NotImplementedException();
+            var primeList = _primeNumberGenerator.Generate(length).ToArray();
+            var result = new int?[length + 1, length + 1];
+            
+            for(int i = 1; i <= length; i++)
+            {
+                result[0, i] = result[i, 0] = primeList[i - 1];
+                for (int j = 1; j <= i; j++)
+                    result[j, i] = result[i, j] = primeList[i - 1] * primeList[j - 1];
+            }
+
+            return result;
         }
     }
 }

5d05e368ba783ed9eb7813878ab8264e3e8d06ef
Isaac Llopis <isaac.llopis.aracil@gmail.com>
2015-11-04 21:01:15 +0000

    Adding missing files.

---
+++ PrimeTable/PrimeTable.Lib/PrimeNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeTable.Lib
+{
+    public class PrimeNumberGenerator
+    {
+        public IEnumerable<int> Generate(int length)
+        {
+            // Validate
+            if (length <= 0)
+                throw new ArgumentOutOfRangeException($"Method {nameof(Generate)} only accepts {nameof(length)} values higher than 0.");
+
+            throw new NotImplementedException("This is a method stub.");
+        }
+    }
+}

9efb11d16845168ee227f0d66cd61dabe45bba88
Isaac Llopis <isaac.llopis.aracil@gmail.com>
2015-11-04 20:34:23 +0000

    Added Generate method test and stub class and method.

---
+++ PrimeTable/PrimeTable.Tests/PrimeNumberGeneratorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeTable.Tests
+{
+    [TestFixture]
+    class PrimeNumberGeneratorTests
+    {
+        private Lib.PrimeNumberGenerator _testObject;
+
+        [SetUp]
+        void BeforeEachTest()
+        {
+            _testObject = new Lib.PrimeNumberGenerator();
+        }
+
+        [TestCase(10)]
+        [TestCase(0, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(-1,ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [Test]
+        void PrimeNumberGenerator_Generate(int length)
+        {
+            // Arrange
+            // Act
+            var result = _testObject.Generate(length);
+
+            // Assert
+            Assert.Equals(length, result.Count());
+        }
+    }
+}

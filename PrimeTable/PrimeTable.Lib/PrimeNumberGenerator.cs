bd118d7533cf79801643d3c0f83b824a1185faca
Isaac Llopis <isaac.llopis.aracil@gmail.com>
2015-11-04 21:17:42 +0000

    Initial implementation of IsPrime according to tests.

--- PrimeTable/PrimeTable.Lib/PrimeNumberGenerator.cs
+++ PrimeTable/PrimeTable.Lib/PrimeNumberGenerator.cs
@@ -9,14 +9,25 @@ namespace PrimeTable.Lib
         {
             // Validate
             if (length <= 0)
-                throw new ArgumentOutOfRangeException($"Method {nameof(Generate)} only accepts {nameof(length)} values higher than 0.");
+                throw new ArgumentOutOfRangeException($"Method {nameof(Generate)} only accepts {nameof(length)} values greater than 0.");
 
             throw new NotImplementedException("This is a method stub.");
         }
 
         public bool IsPrime(int value)
         {
-            throw new NotImplementedException();
+            if (value <= 1)
+                throw new ArgumentOutOfRangeException($"Method {nameof(IsPrime)} only accepts {nameof(value)} values greater than 1.");
+
+            // Special case 2
+            if (value == 2) return true;
+
+            // General case
+            var end = Math.Floor(Math.Sqrt(value));
+            for(int i = 2; i <= end; i++)
+                if (value % i == 0) return false;
+
+            return true;
         }
     }
 }

b6cb4e9f9b0f45dceb50ea8a58b288a1e8b709ce
Isaac Llopis <isaac.llopis.aracil@gmail.com>
2015-11-05 20:26:43 +0000

    Oops. Changed 10 for 11 in test case. It needs to be higher than 10!

--- PrimeTable/PrimeTable.Tests/CLI/PrimeNumbersAppControllerTests.cs
+++ PrimeTable/PrimeTable.Tests/CLI/PrimeNumbersAppControllerTests.cs
@@ -38,7 +38,7 @@ namespace PrimeTable.Tests.CLI
 
         [TestCase(3)]
         [TestCase(0, ExpectedException = typeof(ArgumentOutOfRangeException))]
-        [TestCase(10, ExpectedException = typeof(ArgumentOutOfRangeException))]
+        [TestCase(11, ExpectedException = typeof(ArgumentOutOfRangeException))]
         [Test]
         public void PrimeNumbersAppController_Run(int value)
         {
